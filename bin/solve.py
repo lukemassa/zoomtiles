cb4037add689ed2ec3a0aa1ccd8fc18496ea7c3c
Luke Massa <lmassa@tripadvisor.com>
2020-05-16 18:09:07 -0400

    Removed as_fraction code

--- bin/solve.py
+++ bin/solve.py
@@ -11,16 +11,15 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("n", type=int, help="Number of squares to attempt to fit")
     parser.add_argument("--up-to", action="store_true", default=False, help="Show results in csv format up to n")
-    parser.add_argument("--as-fraction", action="store_true", default=False, help="Show results as a fraction")
     args = parser.parse_args()
 
     if args.up_to:
         for i in range(1, args.n):
-            # TODO: Refactor solve so it can return both as_fraction and not as_fraction
-            print("%s,%s,%s" % (i, square.solve(i, as_fraction=False), square.solve(i, as_fraction=True)))
+            solution = square.solve(i)
+            print("%s,%s,%s" % (i, float(solution), str(solution)))
 
     else:
-        solved = square.solve(args.n, as_fraction=args.as_fraction)
+        solved = square.solve(args.n)
         print("Efficieny of %s tiles is %s" % (args.n, solved))
 
 
